a417543191d87e510a06277923a4f972d75baad9
HeeGon Chae <chaeheegon5858@gmail.com>
2024-09-11 13:39:07 +0900

    Add files via upload

--- ec2_userdata_fastapi/variables.tf
+++ ec2_userdata_fastapi/variables.tf
@@ -1,10 +1,24 @@
-# 미니콘다를 설치하는 유저데이터 모듈
+variable "ami" {
+  description = "ami id for instance"
+  type = string
+}
 
-data "template_file" "miniconda_userdata" {
-  template = <<-EOF
-    wget https://repo.anaconda.com/miniconda/Miniconda3-py310_24.7.1-0-Linux-x86_64.sh -O /tmp/Miniconda3.sh
-    bash /tmp/Miniconda3.sh -b -p /home/ubuntu/miniconda3
-    echo 'export PATH="/home/ubuntu/miniconda3/bin:$PATH"' >> /home/ubuntu/.bashrc
-    source /home/ubuntu/.bashrc
-  EOF
+variable "key_name" {
+  description = "type of instance"
+  type = string
+}
+
+variable "instance_name" {
+  description = "instance name for instance"
+  type = string
+}
+
+variable "security_group_id" {
+  description = "id assign to instance"
+  type = string
+}
+
+variable "instance_type" {
+  description = "instance name for instance"
+  type = string
 }
